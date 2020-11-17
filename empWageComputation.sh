2ecbb2c33633c06164a929fe85eb500d29e30a26
Harinihany <harinihany03@gmail.com>
2020-11-17 15:34:17 +0530

    added :UC-1

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "welcome to employee wage computation program on master branch"
 
+isPresent=1
+random=$(( RANDOM%2 ))
+echo "Random number: "$random
+if(( $random == $isPresent ))
+then
+echo "Employee is Present!"
+else
+echo "Employee is absent!"
+fi
